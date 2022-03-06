83e0cfb5ae2bd76a25270e0edc7dcf940f64ab78
xaviermiles <xaviermiles377@gmail.com>
2022-03-06 13:11:22 +1300

    basic_get is now a wrapper around internal detailed_get function
    Also made entity argument default to empty string in basic_get(), since requests to the API do not require one. detailed_get() was made so that the app can get both the returned data and constructed url from the same function.

--- R/fct_odata_get.R
+++ R/fct_odata_get.R
@@ -1,31 +1,43 @@
 # TODO: allow querying UAT API - argument in get functions?
 #       This would need to switch between get_golem_config & get_golem_secret.
+# TODO: how to effectively inheritParam with unexported functions?
 
 
-#' basic_get
+#' Basic GET request to Stats NZ OData API, with paging when required.
 #'
-#' @description Basic GET request to Stats NZ OData API.
-#'
-#' Derived from https://github.com/StatisticsNZ/open-data-api/
+#' @description Derived from https://github.com/StatisticsNZ/open-data-api/
 #'
 #' @param endpoint API endpoint. Required.
-#' @param entity Data entity. Required.
+#' @param entity Data entity.
 #' @param query Query URL character (not URL-encoded).
 #' @param timeout Timeout for the GET request(s), in seconds.
 #'
 #' @return A data frame containing the requested data.
 #'
-#' @include utils_odata_get.R
 #' @export
-basic_get <- function(endpoint, entity, query = "", timeout = 60) {
-  service <- get_golem_config("service_prd")
-  # TODO: check for NULL service. Here or somewhere else?
-  url <- glue::glue("{service}/{endpoint}/{entity}",
-                    if (query != "") "?{query}" else "") %>%
-    utils::URLencode()
+basic_get <- function(endpoint, entity = "", query = "", timeout = 60) {
+  # TODO: could query be more R style? list of query types rather than just string?
+  detailed_get(endpoint, entity, query, timeout)$value
+}
+
+
+#' @name detailed_get
+#'
+#' @param endpoint API endpoint. Required.
+#' @param entity Data entity.
+#' @param query Query URL character (not URL-encoded).
+#' @param timeout Timeout for the GET request(s), in seconds.
+#'
+#' @return A list of the response's contents.
+#'
+#' @include utils_odata_get.R
+#' @noRd
+detailed_get <- function(endpoint, entity = "", query = "", timeout = 60) {
+  initial_url <- build_basic_url(endpoint, entity, query)
   top_query <- grepl("$top", query, fixed = TRUE)
 
   result <- data.frame()
+  url <- initial_url
   while (!is.null(url)) {
     content <- send_get(url, timeout)
     result <- rbind(result, content$value)
@@ -34,25 +46,44 @@ basic_get <- function(endpoint, entity, query = "", timeout = 60) {
     url <- content$"@odata.nextLink"
   }
 
-  return(result)
+  return(list(value = result, initial_url = initial_url))
 }
 
 
-#' get_catalogue
+#' Constructs request URLs.
 #'
-#' @description Send GET request to catalogue of Stats NZ OData API.
+#' @param endpoint API endpoint. Required.
+#' @param entity Data entity.
+#' @param query Query URL character (not URL-encoded).
 #'
-#' Derived from https://github.com/StatisticsNZ/open-data-api/
+#' @noRd
+build_basic_url <- function(endpoint, entity = "", query = "") {
+  # TODO: check for NULL service. Here or somewhere else?
+  service <- get_golem_config("service_prd")
+  url <- glue::glue("{service}/{endpoint}")
+  if (entity != "")
+    url <- glue::glue("{url}/{entity}")
+  if (query != "")
+    url <- glue::glue("{url}?{query}")
+
+  return(utils::URLencode(url))
+}
+
+
+#' GET the catalogue of Stats NZ OData API.
+#'
+#' @description Derived from https://github.com/StatisticsNZ/open-data-api/
 #'
 #' @param endpoint API endpoint.
 #' @param timeout Timeout for the GET request, in seconds.
 #'
 #' @return A data frame containing the requested catalogue data. This is
-#' generally not a tidy dataset since it uses prefixes to represent some of
+#' generally not a "tidy dataset" since it uses prefixes to represent some of
 #' the more nested content.
 #'
 #' @export
 get_catalogue <- function(endpoint = "data.json", timeout = 60) {
+  # TODO: would this ever need to page??
   service <- get_golem_config("service_prd")
   # TODO: as above re NULL service.
   url <- utils::URLencode(glue::glue("{service}/{endpoint}"))
@@ -61,6 +92,6 @@ get_catalogue <- function(endpoint = "data.json", timeout = 60) {
   if (!"dataset" %in% names(content))
     stop("Response doesn't contain 'dataset'")
 
-  catalogue <- content$dataset %>% tidyr::unnest_longer(distribution)
+  catalogue <- tidyr::unnest_longer(content$dataset, distribution)
   return(catalogue)
 }
